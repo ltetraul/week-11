8204891ef9e860cc10d59babd3ba98e65bf64f2d
ltetraul <143654006+ltetraul@users.noreply.github.com>
2025-11-05 18:18:31 -0500

    1

--- apputil.py
+++ apputil.py
@@ -1,1 +1,16 @@
-# your code here
+import numpy as np
+from sklearn.cluster import KMeans
+
+def kmeans(X, k):
+    """
+    Performs k-means clustering on numerical numpy array X.
+    """
+    #initialize kmeans model
+    kmeans_model = KMeans(n_clusters=k, random_state=42)
+    kmeans_model.fit(X)
+
+    #extract centroids and labels
+    centroids = kmeans_model.cluster_centers_
+    labels = kmeans_model.labels_
+
+    return centroids, labels
